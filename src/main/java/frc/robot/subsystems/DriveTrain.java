7b282784ca0fd2471a1b34a203fd484e5bbb2c8d
George Ling <georgeling@Georges-MBP.fios-router.home>
2020-01-24 21:50:25 -0500

    Created allDrive and slide functions along with some variables.

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -7,18 +7,49 @@
 
 package frc.robot.subsystems;
 
+import com.kauailabs.navx.frc.AHRS;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj.Relay;
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveTrain extends SubsystemBase {
     /**
      * Creates a new ExampleSubsystem.
      */
+    CANSparkMax leftLeader, leftFollower, rightLeader, rightFollower;
+    DifferentialDrive diffDrive;
+    boolean fieldCentric = false;
+    PIDController turningCtrl;
+    double throttleLimit, turnOutput;
+    AHRS navX;
+    Relay spike;
+
+    enum driveSides {
+        LEFT, RIGHT;
+    }
+
     public DriveTrain() {
-        
+        leftLeader = new CANSparkMax(14, MotorType.kBrushless);
+        leftFollower = new CANSparkMax(14, MotorType.kBrushless);
+        rightLeader = new CANSparkMax(14, MotorType.kBrushless);
+        rightFollower = new CANSparkMax(14, MotorType.kBrushless);
+        diffDrive = new DifferentialDrive(leftLeader, rightLeader);
     }
 
     @Override
     public void periodic() {
         // This method will be called once per scheduler run
+    }
+
+    public void allDrive(double x, double y) {
+        // diffDrive.arcadeDrive(xSpeed, zRotation);
+        // diffDrive.arcadeDrive(throttleLimit, zRotation);
+    }
+
+    public void slide(double distance) {
+
     }
 }
