43d0cbf261fc979898825490e9de9a61744aa6f0
George Ling <georgeling@georges-mbp.newton.edu>
2020-01-25 13:36:16 -0500

    Added intake motor to the Intake subsystem

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -4,17 +4,17 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase {
-    /**
-     * Creates a new ExampleSubsystem.
-     */
-    public Intake() {
+    CANSparkMax intakeMotor;
 
+    public Intake() {
+        intakeMotor = new CANSparkMax(5, MotorType.kBrushless);
     }
 
     @Override
