d945c8078a26ba886ce60d9c7815a043073121d7
Ultimat0 <marknazzaro2@gmail.com>
2020-01-27 18:30:12 -0500

    added some constants and fixed up drive train a little

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -11,34 +11,47 @@ import com.kauailabs.navx.frc.AHRS;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.SPI.Port;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 import frc.robot.Robot;
 
 public class DriveTrain extends SubsystemBase {
-    CANSparkMax leftLeader, leftFollower, rightLeader, rightFollower;
+
+    CANSparkMax leftLeader = new CANSparkMax(Constants.LEADER_LEFT_TALON_ID, MotorType.kBrushless);
+    CANSparkMax leftFollower = new CANSparkMax(Constants.FOLLOWER_LEFT_TALON_ID, MotorType.kBrushless);
+    CANSparkMax rightLeader = new CANSparkMax(Constants.LEADER_RIGHT_TALON_ID, MotorType.kBrushless);
+    CANSparkMax rightFollower = new CANSparkMax(Constants.FOLLOWER_RIGHT_TALON_ID, MotorType.kBrushless);
+
+    private final SpeedControllerGroup leftMotors = new SpeedControllerGroup(leftLeader, leftFollower);
+
+    private final SpeedControllerGroup rightMotors = new SpeedControllerGroup(rightLeader, rightFollower);
+
     DifferentialDrive diffDrive;
-    double limitedThrottle;
+    DifferentialDriveOdometry odometry;
+
+    Encoder leftEncoder = new Encoder(Constants.LEFT_ENCODER_PORTS[0], Constants.LEFT_ENCODER_PORTS[1]);
+    Encoder rightEncoder = new Encoder(Constants.RIGHT_ENCODER_PORTS[0], Constants.RIGHT_ENCODER_PORTS[1]);
+
     AHRS navX;
 
+    double limitedThrottle;
+
     public DriveTrain() {
-        leftLeader = new CANSparkMax(1, MotorType.kBrushless);
-        leftFollower = new CANSparkMax(2, MotorType.kBrushless);
-        rightLeader = new CANSparkMax(3, MotorType.kBrushless);
-        rightFollower = new CANSparkMax(4, MotorType.kBrushless);
 
         // TODO: Verify which motors need to be inverted
         // Since we're using DifferentialDrive below, we should not need to invert any,
         // but it doesn't hurt to be explicit.
         leftLeader.setInverted(false);
-        rghtLeader.setInverted(false);
+        rightLeader.setInverted(false);
 
-        // Note: Default is to follow without inverting the follower
-        leftFollower.follow(leftLeader);
-        rightFollower.follow(rightLeader);
-
-        diffDrive = new DifferentialDrive(leftLeader, rightLeader);
+        diffDrive = new DifferentialDrive(leftMotors, rightMotors);
         diffDrive.setSafetyEnabled(false);
 
         navX = new AHRS(Port.kMXP, (byte) 200);
@@ -51,8 +64,18 @@ public class DriveTrain extends SubsystemBase {
         Arrays.asList(leftLeader, leftFollower, rightLeader, rightFollower)
                 .forEach((CANSparkMax spark) -> spark.setIdleMode(IdleMode.kBrake));
 
+        //TODO determine real numbers to use here
         rightLeader.setOpenLoopRampRate(0.0065);
         leftLeader.setOpenLoopRampRate(0.0065);
+
+        ////////////////////////////ODOMETRY SET UP//////////////////////////////////
+
+        leftEncoder.setDistancePerPulse(Constants.DISTANCE_PER_PULSE);
+        rightEncoder.setDistancePerPulse(Constants.DISTANCE_PER_PULSE);
+
+        odometry = new DifferentialDriveOdometry(Rotation2d.fromDegrees(0));
+
+
     }
 
     @Override
