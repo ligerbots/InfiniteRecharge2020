0bb5806cc45488ecf96ec1cebcd50ce638a64fba
Jesse Taube-Perrotta <mr.bossman075@gmail.com>
2020-01-28 19:35:43 -0500

    added shooter motors and encoder

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -8,27 +8,40 @@ package frc.robot.subsystems;
 
 import java.util.Set;
 
+import com.revrobotics.CANEncoder;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.PWM;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import frc.robot.Constants;
+
 public class Shooter extends SubsystemBase {
     CANSparkMax motor1, motor2, motor3;
+    CANEncoder shooterEncoder;
+    CANSparkMax FLUP;
     PWM hoodServo;
     double shootSpeed;
 
     // TODO: Need to add velocity PID for shooter
 
     public Shooter() {
-
+        motor1 = new CANSparkMax(Constants.SHOOTER_ONE_CAN_ID,MotorType.kBrushless);
+        motor2 = new CANSparkMax(Constants.SHOOTER_TWO_CAN_ID,MotorType.kBrushless);
+        motor3 = new CANSparkMax(Constants.SHOOTER_THREE_CAN_ID,MotorType.kBrushless);
+        shooterEncoder = new CANEncoder(motor1);
+        hoodServo = new PWM(Constants.SHOOTER_SERVO_PWM_ID);
+        FLUP = new CANSparkMax(Constants.SHOOTER_FLUP_CAN_ID,MotorType.kBrushless);
+        motor2.follow(motor1);  //  We want motor1 to be master and motor2 and 3 follow the speed of motor1
+        motor3.follow(motor1);  //  ^^^
     }
 
     @Override
     public void periodic() {
-        // This method will be called once per scheduler run
+        // This method will be called once per scheduler run every 20ms
     }
 
     public void shoot(double distance) {
