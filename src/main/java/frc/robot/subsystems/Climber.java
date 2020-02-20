fad75abb8b8425f32dc0fefbc19b318553779eaf
YJ-7274 <yanjunzhao81@gmail.com>
2020-02-20 14:06:26 -0500

    Added the throughbore encoder to Climber.java

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -10,13 +10,15 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-
 public class Climber extends SubsystemBase {
     private final CANSparkMax shoulder, winch; // declare new motors
+    private DutyCycleEncoder throughBore;
 
     public Climber() {
+        throughBore = new DutyCycleEncoder(9);
         shoulder = new CANSparkMax(Constants.SHOULDER_MOTOR_CAN_ID, MotorType.kBrushless); // init motor type and can id
         shoulder.setIdleMode(IdleMode.kBrake);
         winch = new CANSparkMax(Constants.WINCH_MOTOR_CAN_ID, MotorType.kBrushless); // init motor type and can id
