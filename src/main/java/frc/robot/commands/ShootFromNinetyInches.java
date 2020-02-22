b5763ed05c0e57b8a865754cc5198780e3db8f63
Ultimat0 <marknazzaro2@gmail.com>
2020-02-22 02:08:08 -0500

    made hood go back down after shooting

--- src/main/java/frc/robot/commands/ShootFromNinetyInches.java
+++ src/main/java/frc/robot/commands/ShootFromNinetyInches.java
@@ -49,6 +49,7 @@ public class ShootFromNinetyInches extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    shooter.setHood(160);
     carouselCommand.schedule();
   }
 
