605ed67a1992fa45875361f4bdd9f7fdb2895378
YJ-7274 <yanjunzhao81@gmail.com>
2020-03-06 20:22:34 -0500

    Experimental Five Ball Auto

--- src/main/java/frc/robot/FieldMap.java
+++ src/main/java/frc/robot/FieldMap.java
@@ -18,11 +18,11 @@ public class FieldMap {
     public static final double rL2 = robotLength/2.0;
     
     // centerline is the robot starting position
-    public static final double centerPoint = 227.42; // of target
+    public static final double centerPoint = 92.341; // of target
     public static final double startLine = 509.145;
-    public static final double friendlyTrenchX = 296.176; // center line through long axis of trench
-    public static final double enemyTrenchBall1 = 19.574;       // Ball in enemy trench close to our goal
-    public static final double enemyTrenchBall2 = 38.074;       // Other Ball in enemy trench close to our goal
+    public static final double friendlyTrenchY = 25.414; // center line through long axis of trench
+    public static final double enemyTrenchBall1 = 302.016;       // Ball in enemy trench close to our goal
+    public static final double enemyTrenchBall2 = 283.516;       // Other Ball in enemy trench close to our goal
     
 
     public static ArrayList<Translation2d> ballPositions = new ArrayList<Translation2d>();
@@ -40,36 +40,40 @@ public class FieldMap {
 
         // start positions are in terms of the robot center (x,y)
 
-        startPositions[1] = new Pose2d(centerPoint, startLine, new Rotation2d(0.0));        // directly centered
-        startPositions[2] = new Pose2d(centerPoint - 35, startLine, new Rotation2d(0.0));   // offcenter by 35 inches
-        startPositions[3] = new Pose2d(centerPoint + 35, startLine, new Rotation2d(0.0));   // offcenter by -35 inches
-        startPositions[4] = new Pose2d(friendlyTrenchX, startLine, new Rotation2d(0.0));            // facing trench
-
+        startPositions[1] = new Pose2d(startLine, centerPoint,  new Rotation2d(0.0));        // directly centered
+        startPositions[2] = new Pose2d(startLine, centerPoint - 35, new Rotation2d(0.0));   // offcenter by 35 inches
+        startPositions[3] = new Pose2d(startLine, centerPoint + 35, new Rotation2d(0.0));   // offcenter by -35 inches
+        startPositions[4] = new Pose2d(startLine, friendlyTrenchY,  new Rotation2d(0.0));            // facing trench
+        startPositions[5] = new Pose2d(startLine, 220.341, new Rotation2d(0.0)); // centered in front of enemy feeder station
         //locations of balls
 
-        ballPositions.add(new Translation2d(friendlyTrenchX, 386.688));
-        ballPositions.add(new Translation2d(friendlyTrenchX, 350.688));
-        ballPositions.add(new Translation2d(friendlyTrenchX, 314.688));
-        ballPositions.add(new Translation2d(enemyTrenchBall1, 378.959));
+        ballPositions.add(new Translation2d( 386.688, friendlyTrenchY));
+        ballPositions.add(new Translation2d( 350.688, friendlyTrenchY));
+        ballPositions.add(new Translation2d( 314.688, friendlyTrenchY));
+        ballPositions.add(new Translation2d( 378.959, enemyTrenchBall1));
+        ballPositions.add(new Translation2d( 378.959, enemyTrenchBall2));
 
-
-        /*
         
-        YJ's original ball positions. We may add the rest of these later
+        
+       
 
-        ballPosition[1] = new Translation2d(19.574, 378.959); 
-        ballPosition[2] = new Translation2d(38.074, 378.959);
-        ballPosition[3] = new Translation2d(146.281, 388.637);
-        ballPosition[4] = new Translation2d(161.583, 394.975);
-        ballPosition[5] = new Translation2d(176.884, 401.313);
-        ballPosition[6] = new Translation2d(201.291, 394.200);
-        ballPosition[7] = new Translation2d(207.630, 378.899);
-        ballPosition[8] = new Translation2d(296.176, 386.688);
-        ballPosition[9] = new Translation2d(296.176, 350.688);
-        ballPosition[11] = new Translation2d(286.926, 250.417);
-        ballPosition[12] = new Translation2d(305.426, 250.417);
+        //ballPosition[1] = new Translation2d(378.959);  //already added in
+        //ballPosition[2] = new Translation2d(378.959);  //already added in
+        //ballPosition[8] = new Translation2d(386.688, 25.414);     //already added in
+        //ballPosition[9] = new Translation2d(350.688, 25.414);     //already added in
+        //ballPosition[10] = new Translation2d(314.688, 25.414);
 
-        */
+        
+        //THESE NEED TO BE ADDED 
+        //ballPosition[3] = new Translation2d(388.637, 173.480); 
+        //ballPosition[4] = new Translation2d(394.975, 158.178);                            
+        //ballPosition[5] = new Translation2d(401.313, 142.877 );
+        //ballPosition[6] = new Translation2d(394.200, 118.470);
+        //ballPosition[7] = new Translation2d(378.899, 112.131); 
+        //ballPosition[11] = new Translation2d(250.417, 34.664);
+        //ballPositions[12] = new Translation2d(250.417,16.164);
+
+        
 
     };
 
---
+++ src/main/java/frc/robot/commands/FiveBallCenterAuto.java
@@ -0,0 +1,91 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import java.util.List;
+
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj.controller.RamseteController;
+import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward;
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.geometry.Translation2d;
+import edu.wpi.first.wpilibj.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryConfig;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj.trajectory.constraint.DifferentialDriveVoltageConstraint;
+import edu.wpi.first.wpilibj2.command.RamseteCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants;
+import frc.robot.subsystems.Carousel;
+import frc.robot.subsystems.Climber;
+import frc.robot.subsystems.DriveTrain;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Shooter;
+
+public class FiveBallCenterAuto extends SequentialCommandGroup {
+  /**
+   * Add your docs here.
+   */
+  public FiveBallCenterAuto(final DriveTrain robotDrive, final Shooter shooter, final Intake intake, final Climber climber, final Carousel carousel, final DriveCommand driveCommand) {
+    driveCommand.cancel();
+    // intake.run(0.4);
+    final IntakeCommand intakeCommand = new IntakeCommand(intake, climber, Constants.INTAKE_SPEED);
+    robotDrive.resetOdometry(new Pose2d());
+    final var autoVoltageConstraint =
+        new DifferentialDriveVoltageConstraint(
+            new SimpleMotorFeedforward(Constants.ksVolts,
+                                       Constants.kvVoltSecondsPerMeter,
+                                       Constants.kaVoltSecondsSquaredPerMeter),
+            Constants.kDriveKinematics,
+            10);
+    final TrajectoryConfig config =
+      new TrajectoryConfig(Constants.kMaxSpeed,
+                            Constants.kMaxAcceleration)
+          // Add kinematics to ensure max speed is actually obeyed
+          .setKinematics(Constants.kDriveKinematics)
+          // Apply the voltage constraint
+          .addConstraint(autoVoltageConstraint);
+    final Trajectory fiveBallTrajectory = TrajectoryGenerator.generateTrajectory(
+      // Start at the origin facing the +X direction
+      new Pose2d(0, 0, new Rotation2d(0)),
+      // Pass through these two interior waypoints, making an 's' curve path
+      List.of(
+         new Translation2d(388.637 * Constants.inchToMetersConversionFactor, 173.480 * Constants.inchToMetersConversionFactor),
+         new Translation2d(394.975 * Constants.inchToMetersConversionFactor, 158.178 * Constants.inchToMetersConversionFactor),                            
+         new Translation2d(401.313 * Constants.inchToMetersConversionFactor, 142.877 * Constants.inchToMetersConversionFactor),
+         new Translation2d(394.200 * Constants.inchToMetersConversionFactor, 118.470 * Constants.inchToMetersConversionFactor),
+         new Translation2d(378.899 * Constants.inchToMetersConversionFactor, 112.131 * Constants.inchToMetersConversionFactor) 
+      ),
+      // End 3 meters straight ahead of where we started, facing forward
+      new Pose2d(122.1 * Constants.inchToMetersConversionFactor, 0, new Rotation2d(0)),
+      // Pass config
+      config
+      );
+
+
+    final RamseteCommand ramseteCommand3 = new RamseteCommand(
+        fiveBallTrajectory,
+        robotDrive::getPose,
+        new RamseteController(Constants.kRamseteB, Constants.kRamseteZeta),
+        new SimpleMotorFeedforward(Constants.ksVolts,
+                                   Constants.kvVoltSecondsPerMeter,
+                                   Constants.kaVoltSecondsSquaredPerMeter),
+        Constants.kDriveKinematics,
+        robotDrive::getWheelSpeeds,
+        new PIDController(Constants.kPDriveVel, 0, 0),
+        new PIDController(Constants.kPDriveVel, 0, 0),
+        robotDrive::tankDriveVolts,
+        robotDrive
+    );
+
+    addCommands(intakeCommand.alongWith(ramseteCommand3), andThen(() -> robotDrive.tankDriveVolts(0, 0)));//new StartMatchCommand(), new ShooterCommand (shooter, carousel, robotDrive, 3.0));
+    
+  }
+
+}
