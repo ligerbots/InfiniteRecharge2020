1017b21234d142aae2ab490c8f77d8ebe2bfabd5
coachFitz0807 <j-fitz@rcn.com>
2020-11-23 19:47:54 -0500

    fix AutoComandInterface to extend Comand

--- src/main/java/frc/robot/commands/AutoCommandInterface.java
+++ src/main/java/frc/robot/commands/AutoCommandInterface.java
@@ -8,19 +8,4 @@ import edu.wpi.first.wpilibj2.command.Command;
 public interface AutoCommandInterface extends Command {
     public Pose2d getInitialPose();
 
-    // This also needs to have all the methods of a Command.
-    // The commands that implement this interface will all have implementations of
-    // these methods
-    public void initialize();
-
-    public void execute();
-
-    public void end(boolean isInterrupted);
-
-    public boolean isFinished();
-
-    // public void isInterrupted();
-    default void schedule() {
-        Command.super.schedule();
-    };
 }
